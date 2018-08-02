8a7b1d1f6f8591c15c597566b606f306990a906d
Mikkel Hartmann Jensen <mikkelheart@gmail.com>
2018-08-02 19:29:02 +0200

    Cleaned the notebook a bit to better allign with my blogpost about it.

--- notebooks/helperFunctions.py
+++ notebooks/helperFunctions.py
@@ -1,10 +1,3 @@
 def some_function():
     print('Printing something.')
     print('Now it is printing some more.')
-    print('Now it is printing some more.')
-    print('Now it is printing some more.')
-    print('Now it is printing some more.')
-    print('Now it is printing some more.')
-    print('Now it is printing some more.')
-    print('Now it is printing some more.')
-    print('Now it is printing some more.')
