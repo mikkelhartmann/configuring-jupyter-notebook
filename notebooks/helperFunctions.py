9a500f577fd10916794208e70c6bc2ade8b96afa
Mikkel Hartmann Jensen <mikkelheart@gmail.com>
2018-08-14 11:44:36 +0200

    Added the option of placing content in the margin. This is the first draft of a solution. The positioning is a bit hacky.

--- notebooks/helperFunctions.py
+++ notebooks/helperFunctions.py
@@ -1,3 +1,11 @@
 def some_function():
     print('Printing something.')
     print('Now it is printing some more.')
+    print('Now it is printing some more.')
+    print('Now it is printing some more.')
+    print('Now it is printing some more.')
+    print('Now it is printing some more.')
+    print('Now it is printing some more.')
+    print('Now it is printing some more.')
+    print('Now it is printing some more.')
+    print('Now it is printing some more.')
